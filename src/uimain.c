ee18c929a03dd83165efd28cb2c754247dc30fb6
Angel Rodriguez II <rodriguez4174@gmail.com>
2021-03-01 20:11:48 -0700

    ui needs work

--- src/uimain.c
+++ src/uimain.c
@@ -1,29 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
+
+int string_compare(char*x, char *y){
+  while (*x != '\0' || *y != '\0'){
+    if (*x == *y){
+      x++;
+      y++;
+    }else{
+      return 0;
+    }
+    return 1;
+  }
+}
 
 int main(){
   
+  char in[100];
   char str[100];
+  List *list = init_history();
+
+  add_history(list, "happy alone");
+  add_history(list, "spiral staircase");
+  print_history(list);
   
-  printf(">");
-  fgets(str, 100, stdin);
-  printf("%s", str);
+  void menu(){
+    /*printf(">");
+    fgets(str, 100, stdin);
+    printf("%s", str);
+    //scanf("%s", str);
+    //printf("%s\n", str);
 
-  char ** input = tokenize(str);
-  print_tokens(input);
-  free_tokens(input);
+    char ** input = tokenize(str);
+    add_history(list, str);
+    print_tokens(input);
+    free_tokens(input);*/
+    
+    }
 
-  return 0;
+  
+  /* while(1){
+      printf("------menu------\n");
+      printf("a. enter a string\n");
+      printf("b. print history\n");
+      printf("q.quit\n");
+      //scanf("%s", in);
+      fgets(in, 100, stdin);
+      printf("you entered: '%s'\n", in);
 
-  //printf("%d\n", space_char('f'));
-  //printf("%d\n", space_char(' '));
-  //printf("%d\n", non_space_char('f'));
-  //printf("%d\n", non_space_char(' '));
-  //printf("%s\n", word_start(str));
-  //printf("%s\n", word_terminator(str));
-  //printf("%d\n", count_words(str));
-  //char ** a = tokenize(str);
-  //print_tokens(a);
-  //free_tokens(a);
+    //if(input[0] == '!'){
+      
+    
+      if (string_compare(in, "a")){
+	printf("enter your string below:\n");
+	printf(">");
+	fgets(str, 100, stdin);
+	printf("%s", str);
+
+	char ** input = tokenize(str);
+	add_history(list, str);
+	print_tokens(input);
+	//free_tokens(input);
+      }
+      if(string_compare(in, "b")){
+	printf("------history-------\n");
+	print_history(list);
+      }
+      if(string_compare(in, "q")){
+	printf("goodbye\n");
+	goto done;
+      }
+    }
+ done:
+ return 0;*/
 }
